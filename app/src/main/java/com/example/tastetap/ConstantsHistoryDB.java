4ce55413de2a58fc7bf0ec6329931bf11a1e85a3
Kamonchanok Klayjinda <kamonchanok.klayjinda@gmail.com>
2023-12-22 18:09:53 +0700

    add Thai language

--- app/src/main/java/com/example/tastetap/ConstantsHistoryDB.java
+++
@@ -1,9 +0,0 @@
-package com.example.tastetap;
-
-import android.provider.BaseColumns;
-
-public interface ConstantsHistoryDB extends BaseColumns {
-    public static final String TABLE_NAME = "History";
-    public static final String COL_NAME = "Name";
-    public static final String COL_CAL = "Calories";
-}
